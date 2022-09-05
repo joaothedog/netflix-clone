7f0986fabf34bddc203a6f6733e960bce246536f
João Oliveira <jvcbatist4@outlook.com>
2022-09-05 17:23:15 -0300

    Adicionado a lista de filmes e o filme em destaque

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import './App.css';
+import Tmdb from './Tmdb';
+import { useEffect, useState } from 'react';
+import MovieRow from './components/MovieRow';
+import HighlightedMovie from './components/HighlightedMovie';
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [highlightedData, setHighlightedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista toda
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Filme em destaque
+      let originals = list.filter(item => item.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setHighlightedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <h1>Olá Mundo</h1>
+    <div className="page">
+
+      {highlightedData &&
+        <HighlightedMovie item={highlightedData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/HighlightedMovie.js
@@ -0,0 +1,36 @@
+import './HighlightedMovie.css';
+
+export default ({ item }) => {
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
+
+  return (
+    <section className="highlighted" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+    }}>
+      <div className='highlighted-vertical-fade'>
+        <div className='highlighted-horizontal-fade'>
+          <div className='highlighted-name'>{item.original_name}</div>
+          <div className='highlighted-info'>
+            <div className='highlighted-points'>{item.vote_average.toFixed(1)} pontos</div>
+            <div className='highlighted-year'>{firstDate.getFullYear()}</div>
+            <div className='highlighted-seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} disponí{item.number_of_seasons !== 1 ? 'veis' : 'vel'}</div>
+          </div>
+          <div className='highlighted-description'>{item.overview}</div>
+          <div className='highlighted-btns'>
+            <a href={`/watch/${item.id}`} className='highlighted-watch-btn'>▶ Assistir</a>
+            <a href={`/list/add/${item.id}`} className='highlighted-addmylist-btn'>+ Minha lista</a>
+
+          </div>
+          <div className='highlighted-genres'><strong>Gêneros:</strong> {genres.join(', ')}</div>
+        </div>
+      </div>
+
+    </section >
+  );
+}
