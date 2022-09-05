72561b952a939a4118fa7d4f460ad43171711318
João Oliveira <jvcbatist4@outlook.com>
2022-09-05 19:36:18 -0300

    Finalizando projeto

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,7 @@ import './Header.css';
 
 export default ({ black, hiddenh }) => {
   return (
-    <header className={
-      hiddenh ? 'hiddenheader' : '' + black ? 'black' : ''}>
+    <header className={hiddenh ? 'hiddenheader' : '' + black ? 'black' : ''}>
       <div className='header-logo'>
         <a href='/'>
           <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt='Netflix' />
